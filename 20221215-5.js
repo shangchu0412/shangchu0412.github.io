3f11a9367db7aba58e19c24557b5a912e85b72a9
shangchu0412 <shangchu0412@gmail.com>
2022-12-19 10:39:57 +0800

    v1.06

---
+++ 20221215-5.js
@@ -0,0 +1,48 @@
+/*=====================================
+  ex
+=====================================*/
+function test() {
+  let item = {
+    width: '3',
+    label: '姓名',
+    type: "text",
+    name: "name",
+    value: "",
+    valid: "required",
+    option: "單選1|單選2|單選3"
+  };
+
+  let form_item = [
+    {
+      width: '3',
+      label: '姓名',
+      type: "text",
+      name: "name",
+      value: "",
+      valid: "required",
+      option: ""
+    },
+    {
+      width: '3',
+      label: '網址',
+      type: "url",
+      name: "url",
+      value: "",
+      valid: "required",
+      option: ""
+    },
+    {
+      width: '3',
+      label: 'EMAIL',
+      type: "email",
+      name: "email",
+      value: "",
+      valid: "",
+      option: ""
+    }]
+
+  console.log(form_item[0].label);
+
+  console.log(form_item[0]['label']);
+
+}
